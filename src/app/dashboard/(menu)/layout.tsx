3b79ec62af7f72c0d9f4e35ab8e2b73517aff7a0
suman adhikari <yes.u.man7@gmail.com>
2025-02-13 08:26:49 +0545

    Iframe updated

--- src/app/dashboard/(menu)/layout.tsx
+++ src/app/dashboard/(menu)/layout.tsx
@@ -14,7 +14,7 @@ export default function MenuPageLayout({
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="flex-grow  md:overflow-y-auto ">{children}</div>
-      <div className="bg-gray-400 h-screen flex-none md:w-96 justify-center items-center flex overflow-x-hidden  ">
+      <div className="bg-gray-200 h-screen flex-none md:w-96 justify-center items-center flex overflow-x-hidden  ">
         <iframe
           src="/menu"
           className="w-[330px] h-[600px] border rounded-xl shadow-lg bg-red-200"
