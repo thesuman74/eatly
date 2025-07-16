e4f9062822a6f88528443dcf22aaa8490c058ced
suman adhikari <yes.u.man7@gmail.com>
2025-07-16 14:04:04 +0545

    Zustand setup completed

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,55 +1,28 @@
 "use client";
 
-import {
-  Check,
-  CheckCheck,
-  Cross,
-  DollarSign,
-  Eye,
-  Filter,
-  X,
-} from "lucide-react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import CounterTable from "@/components/order/CounterTable";
-import OrderTabsSection from "@/components/order/TabsSection";
-export default function Home() {
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-  ];
+// src/components/pages/home-page.tsx
 
-  return (
-    <>
-      <div className="w-full">
-        <OrderTabsSection />
-      </div>
-      {/* <CounterTable /> */}
-    </>
+import React from "react";
+import { useCounterStore } from "@/components/HOC/ProductCartProvider";
+
+const page = () => {
+  const { count, incrementCount, decrementCount } = useCounterStore(
+    (state) => state
   );
-}
+  return (
+    <div>
+      <div>
+        Count: {count}
+        <hr />
+        <button type="button" onClick={incrementCount}>
+          Increment Count
+        </button>
+        <button type="button" onClick={decrementCount}>
+          Decrement Count
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/components/HOC/Providers.tsx
+++ src/components/HOC/Providers.tsx
@@ -6,13 +6,16 @@ import ToastProvider from "./ToastProvider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CounterStoreProvider } from "./ProductCartProvider";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ToastProvider>{children}</ToastProvider>
+      <ToastProvider>
+        <CounterStoreProvider>{children}</CounterStoreProvider>
+      </ToastProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
