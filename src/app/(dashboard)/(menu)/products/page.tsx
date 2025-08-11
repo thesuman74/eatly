6c5aa7512a2c57442b15f59c292c00504c095e52
suman adhikari <yes.u.man7@gmail.com>
2025-08-11 12:39:48 +0545

    force dynamic

--- src/app/(dashboard)/(menu)/products/page.tsx
+++ src/app/(dashboard)/(menu)/products/page.tsx
@@ -3,6 +3,8 @@ import { DragAndDropProvider } from "@/components/dashboard/DragAndDrop/DragAndD
 import TopSection from "@/components/menu/TopSection";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
   const res = await fetch(`${baseUrl}/api/menu/structured`);
