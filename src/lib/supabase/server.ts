bec027e358c0a45a6d8cd58d0c9c6117ff3918a7
suman adhikari <yes.u.man7@gmail.com>
2025-12-16 22:51:17 +0545

    Added additional check

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,6 +4,10 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
+  if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error("Missing Supabase anon key");
+  }
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     // process.env.SUPABASE_SERVICE_ROLE_KEY!,
