54bd9991026baa910fce7de9a66a2ff4aeec999c
suman adhikari <yes.u.man7@gmail.com>
2026-01-20 01:10:27 +0545

    removed unnecessary button

--- src/app/dashboard/[restaurantId]/order/_components/Products/EditableOrderItemsList.tsx
+++ src/app/dashboard/[restaurantId]/order/_components/Products/EditableOrderItemsList.tsx
@@ -52,17 +52,6 @@ export default function EditableOrderItemsList({
         >
           + Products
         </span>
-        {/* Update Cart Button */}
-        {modifiedItems.length > 0 && paymentStatus === "unpaid" && (
-          <div className="p-2  flex justify-end">
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-              onClick={handleUpdateCart}
-            >
-              Update Cart
-            </button>
-          </div>
-        )}
       </div>
 
       {/* Scrollable Items List */}
